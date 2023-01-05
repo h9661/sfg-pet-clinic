b9a045f448457a82c583dfe0f57052415b013120
h9661 <guscksdn123@gmail.com>
2023-01-05 12:45:58 +0900

    added visits, updated owner with pet relationship. closes #26

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,5 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person{
 
+    private Set<PetType> pets;
+
+    public Set<PetType> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<PetType> pets) {
+        this.pets = pets;
+    }
 }
