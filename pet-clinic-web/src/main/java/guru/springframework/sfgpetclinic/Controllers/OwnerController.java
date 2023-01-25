c1a73407d532a3cf03f297f69dc2e28bba2cdc02
h9661 <guscksdn123@gmail.com>
2023-01-25 20:19:40 +0900

    owner의 세부 사항을 볼 수 있는 html 파일과 그것을 처리하는 부분을 OwnerController에 추가하였다. Closes #53

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/Controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/Controllers/OwnerController.java
@@ -1,8 +1,11 @@
 package guru.springframework.sfgpetclinic.Controllers;
 
+import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
@@ -21,8 +24,11 @@ public class OwnerController {
         return "owners/index";
     }
 
-    @RequestMapping("/find")
-    public String findOwners(){
-        return "temp";
+
+    @GetMapping("/{ownerId}")
+    public String showOwner(@PathVariable("ownerId") String ownerId, Model model){
+        model.addAttribute("owner", ownerService.findById(Long.valueOf(ownerId)));
+
+        return "owners/ownerDetails";
     }
 }
