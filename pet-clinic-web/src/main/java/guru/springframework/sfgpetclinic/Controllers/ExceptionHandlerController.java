4f291da8b3fd130d90acbb3cd5c098a42224b42f
h9661 <guscksdn123@gmail.com>
2023-02-01 13:55:57 +0900

    impl handle Not Found exception method. #76

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/Controllers/ExceptionHandlerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/Controllers/ExceptionHandlerController.java
@@ -1,12 +1,24 @@
 package guru.springframework.sfgpetclinic.Controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.webjars.NotFoundException;
 
 @Slf4j
 @ControllerAdvice
 public class ExceptionHandlerController {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public String handleNotFoundError(Exception exception, Model model){
+        log.error("handling NotFoundError");
+        log.error(exception.getMessage());
 
+        model.addAttribute("exception", exception);
 
+        return "404error";
+    }
 }
