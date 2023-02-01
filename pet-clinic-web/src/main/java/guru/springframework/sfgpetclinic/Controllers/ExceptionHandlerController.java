4bdac2779179ed8485633d3f6180913cb5e79df2
h9661 <guscksdn123@gmail.com>
2023-02-01 13:30:35 +0900

    created ExceptionHandlerController class with @ControllerAdvice annotation. #76

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/Controllers/ExceptionHandlerController.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic.Controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@Slf4j
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+
+}
